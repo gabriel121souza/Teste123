990c20bd1a34ff76998dbfb984ba010e0d6a8836
Gabriel de Souza <gabriel121souza@gmail.com>
2019-07-13 13:45:41 -0300

    teste execultavel

---
+++ entites/Comment.java
@@ -0,0 +1,23 @@
+
+package entites;
+
+
+public class Comment {
+    private String text;
+
+    public Comment(String comment) {
+        this.text = comment;
+        
+        
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    
+}
